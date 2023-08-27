af4f0f2374144f3a6d9c46ae1ba15807a138a597
rihhana <hrihhana@gmail.com>
2023-08-27 23:25:14 +0300

    done it

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const countBtn = document.querySelector('#count-btn');
+const inputText = document.querySelector('#input-text');
+const vowelCount = document.querySelector('#vowel-count');
+
+countBtn.addEventListener('click', () => {
+  const text = inputText.value;
+  const vowels = text.match(/[aeiou]/gi);
+  vowelCount.innerText = vowels ? vowels.length : 0;
+
+  const result = `Total Vowels: ${vowelCount.innerText}`;
+  document.querySelector('#result').innerText = result;
+});
 
